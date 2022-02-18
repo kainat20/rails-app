55139e2ca8cba6d80e4a2ded02c91a9c391b1488
Rehan Zia <im.geniusman@gmail.com>
2022-02-18 14:48:18 +0000

    Associated ShippingAddress with Orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,11 +2,11 @@
 
 class Order < ApplicationRecord
   belongs_to :customer
+  has_one :shipping_address, as: :addressable, class_name: 'ShippingAddress', dependent: :destroy
   has_many :line_items, dependent: :destroy
 
-  validates :number, :date, :payment_type, presence: true
+  validates :number, :date, :payment_type, :line_items, :shipping_address, presence: true
   validates :number, uniqueness: true
-  validates :line_items, presence: true
 
   enum payment_type: { cash: 'cash', credit_card: 'credit_card', check: 'check', other: 'other' }
 
